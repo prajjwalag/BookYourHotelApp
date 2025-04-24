45be37f1e671779f4f4a18069abc4cffa96a93a0
Prajjwal <prajjwal.agrahari16@gmail.com>
2025-04-24 22:01:29 +0530

    Initialized the App. Made Header, Footer, Hero, Trending Destination and Home Page. Did a lot of configurations

---
+++ src/app/index.jsx
@@ -0,0 +1,17 @@
+import Footer from '@/components/layouts/footer.layout'
+import Header from '@/components/layouts/header.layout'
+import Home from './home'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Home/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default App
